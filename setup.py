a18b3e0167cc9ad5b495428e6cf9f3dffa5ef9cc
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2019-12-27 21:58:07 +0100

    Updated shields in README.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
 
 setuptools.setup(
 	name=projectName,
-	version="0.1.4",
+	version="0.1.5",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",
