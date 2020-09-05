411c9fa8bdeb88026333209722c54ec730fa1ddc
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2020-09-05 20:36:39 +0200

    Bump version to trigger a release.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ rtd_url =     "https://" + projectName + ".readthedocs.io/en/latest/"
 
 setuptools.setup(
 	name=projectName,
-	version="0.1.8",
+	version="0.1.9",
 
 	author="Patrick Lehmann",
 	author_email="Paebbels@gmail.com",
