c66e08aad8d1609940b531f746c0ba212c17db0a
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-12-16 08:49:12 +0100

    Bump version to v0.2.4.

--- pyTooling/GenericPath/__init__.py
+++ pyTooling/GenericPath/__init__.py
@@ -40,7 +40,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2017-2021, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.2.3"
+__version__ =   "0.2.4"
 __keywords__ =  ["path", "generic path", "generic library", "url"]
 
 from typing import List
